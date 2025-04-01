c704ce60a1da9cbc4189b421c5a46f8797c1d6b1
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2025-04-01 18:39:23 -0400

    deleted commented-out code

--- src/MovieDetails.jsx
+++ src/MovieDetails.jsx
@@ -1,8 +1,6 @@
 function MovieDetails({ movie }) {
     return (
         <div className="movie-details">
-            {/* <h2>{movie.Title}</h2> */}
-            {/* <img src={movie.Poster !== 'N/A' ? movie.Poster : 'placeholder.jpg'} alt={movie.Title} /> */}
             <p><strong>Genre:</strong> {movie.Genre}</p>
             <p><strong>Plot:</strong> {movie.Plot}</p>
             <p><strong>IMDB Rating:</strong> {movie.imdbRating}</p>
